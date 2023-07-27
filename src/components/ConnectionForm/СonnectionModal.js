5e9d1cb492fab95d07ef2158e3b8f05f858ad4fd
IraAmber <nute7021@gmail.com>
2023-07-27 14:39:26 +0300

    correct method netlify

--- src/components/ConnectionForm/СonnectionModal.js
+++ src/components/ConnectionForm/СonnectionModal.js
@@ -197,7 +197,7 @@ const ConnectionForm = ({ isOpen, onClose }) => {
                 data-netlify="true"
                 method="post"
                 onSubmit={handleSubmit} // Додали обробник onSubmit
-                netlify
+                netlify="true"
               >
                 <input type="hidden" name="form-name" value="connection-form" />
                 <div>
